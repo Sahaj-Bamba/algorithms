24f33ffc444cea3085dab6fa159d7f6f13016a6d
woc2020 <64241340+woc2020@users.noreply.github.com>
2020-10-24 18:51:15 +0530

    Create EquilibriumPoint.cpp

---
+++ standard questions/EquilibriumPoint.cpp
@@ -0,0 +1,22 @@
+int equilibrium(int arr[], int n)  
+{  
+    int sum = 0; // initialize sum of whole array  
+    int leftsum = 0; // initialize leftsum  
+  
+    /* Find sum of the whole array */
+    for (int i = 0; i < n; ++i)  
+        sum += arr[i];  
+  
+    for (int i = 0; i < n; ++i)  
+    {  
+        sum -= arr[i]; // sum is now right sum for index i  
+  
+        if (leftsum == sum)  
+            return i;  
+  
+        leftsum += arr[i];  
+    }  
+  
+    /* If no equilibrium index found, then return 0 */
+    return -1;  
+}  
