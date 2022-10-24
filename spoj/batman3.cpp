6b83d4812471d43e852748e46f84ea59420c4a08
Sneha Bamba <56398135+SnehaBamba@users.noreply.github.com>
2022-10-24 11:24:47 +0530

    code cleanup
    comments deleted and spaces

--- spoj/batman3.cpp
+++ spoj/batman3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define size1 1005
  
 int dp[size1][size1],a[size1],n,rom;
- 
+
 int fun(int d,int idx){
     if(idx>n){
         return 0;
@@ -12,17 +12,15 @@ int fun(int d,int idx){
     if(dp[d][idx] != -1){
         return dp[d][idx];
     }
- 
     int a1,a2,a3;
     a1 = a2 = a3 =0;
     a2 = fun(d,idx+1);
- 
     if(a[idx]<a[d] || d == 0 || (a[idx] >= a[d] && d == rom)){
         a1 = 1 + fun(idx,idx+1);
     }
-    //cout<<"a1 = "<<a1<<" a2 = "<<a2<<" a3 = "<<a3<<endl;
     return dp[d][idx] = max(a1,a2);
 }
+
 int main()
 {
     int i,t;
@@ -35,8 +33,6 @@ int main()
             cin>>a[i];
         }
         cout<<fun(0,1)<<endl;
-       // cin>>n;
     }
- 
     return 0;
 }
